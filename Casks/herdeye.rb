c20f8695bc5bd7b80cf8cfad987f5bd65848de7c
shogoisaji <simacsimac9@gmail.com>
2026-07-24 12:27:39 +0900

    Bump HerdEye cask to 0.1.3

--- Casks/herdeye.rb
+++ Casks/herdeye.rb
@@ -1,6 +1,6 @@
 cask "herdeye" do
-  version "0.1.2"
-  sha256 "783e799a048e88d0cebe7cc76084834559137241a36b3b942fecf5610b7e0737"
+  version "0.1.3"
+  sha256 "983c2fb0e25501cb60efec3364c194bac990157bbb5f45656f6477a06465413e"
 
   url "https://github.com/shogoisaji/HerdEye/releases/download/v#{version}/HerdEye-#{version}.zip"
   name "HerdEye"
